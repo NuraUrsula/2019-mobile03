019f8fa1feac3dec330b78274689a44ab2332a69
NuraUrsula <nuraursula980@gmail.com>
2019-09-22 15:08:34 +0700

    Welcome Activity, menambah Gambar Character dengan Calender Text Welcome,Sub Text Welcome,Text Login,Background Footer Merah.

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class WelcomeActivity extends AppCompatActivity {
+class WelcomeSlide extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
