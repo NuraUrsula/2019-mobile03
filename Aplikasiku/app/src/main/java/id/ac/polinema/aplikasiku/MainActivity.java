8a31b6dd77b21671bbb36daef79e1b13f19f1201
Lalak <42990337+NuraUrsula@users.noreply.github.com>
2019-09-22 15:16:39 +0700

    Update MainActivity.java

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
@@ -8,17 +8,17 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				Intent i = new Intent(MainActivity.this, WelcomeActivity.class);
-				startActivity(i);
-				finish();
-			}
-		}, 1000);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, WelcomeSlide.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
 }
